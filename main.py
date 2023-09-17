ccb065a1d460512a0d0311b373e3a7f3a43a43ad
Germiprogramer <llorentemunozgerman@gmail.com>
2023-09-17 17:49:50 +0200

    commit

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ solver = pywraplp.Solver('Maximize army power', pywraplp.Solver.GLOP_LINEAR_PROG
 
 #Variables (x,y,z)
 
-x = solver.IntVar(0, solver.infinity(), 'espadachin')
-y = solver.IntVar(0, solver.infinity(), 'arquero')
-z = solver.IntVar(0, solver.infinity(), 'jinete')
+x1 = solver.IntVar(0, solver.infinity(), 'espadachin')
+x2 = solver.IntVar(0, solver.infinity(), 'arquero')
+x3 = solver.IntVar(0, solver.infinity(), 'jinete')
 
 #Restricciones
 
-solver.Add(60*x + 80*y + 140*z <= 1200)
-solver.Add(20*x + 10*y <= 800)
-solver.Add(40*y + 100*z <= 600)
+solver.Add(60*x1 + 80*x2 + 140*x3 <= 1200)
+solver.Add(20*x1 + 10*x2 <= 800)
+solver.Add(40*x2 + 100*x3 <= 600)
 
 #Maximizar la función objetivo
 
-solver.Maximize(70*x + 95*y + 230*z)
+solver.Maximize(70*x1 + 95*x2 + 230*x3)
 
 #Resolver el sistema
 
@@ -28,9 +28,9 @@ status = solver.Solve()
 if status == pywraplp.Solver.OPTIMAL:
 
     print('Solución:')
-    print('Cantidad de espadachines =', x.solution_value())
-    print('Cantidad de arqueros =', y.solution_value())
-    print('Cantidad de jinetes =', z.solution_value())
+    print('Cantidad de espadachines =', x1.solution_value())
+    print('Cantidad de arqueros =', x2.solution_value())
+    print('Cantidad de jinetes =', x3.solution_value())
     print('Fuerza óptima de la armada =', solver.Objective().Value())
 
 else:
